98d14b9cc94f6de10a0b247884f3b06f8a1903ee
Omar Fetooh <omarfetooh62@gmail.com>
2025-01-09 12:29:22 +0200

    Complete cart CRUD

--- src/modules/Cart/cart.controller.js
+++ src/modules/Cart/cart.controller.js
@@ -55,3 +55,86 @@ export const addCart = async (req, res, next) => {
     cart,
   });
 };
+
+export const updateCart = async (req, res, next) => {
+  const { productId } = req.params;
+  const { quantity } = req.body;
+  const userId = req.authUser._id;
+
+  const product = await Product.findOne({
+    _id: productId,
+    stock: { $gte: quantity },
+  });
+
+  if (!product) {
+    return next(new ErrorClass("product not found or out of stock", 400));
+  }
+
+  const cart = await Cart.findOne({ userId, "products.productId": productId });
+  if (!cart) {
+    return next(new ErrorClass("Product not in cart", 400));
+  }
+
+  const productIndex = cart.products.findIndex((p) => p.productId == productId);
+  cart.products[productIndex].quantity = quantity;
+
+  const subTotal = cart.products.reduce(
+    (total, p) => total + p.quantity * p.price,
+    0
+  );
+  cart.subTotal = subTotal;
+
+  await cart.save();
+
+  return res.status(200).json({
+    status: "Success",
+    message: "Cart Updated Successfully",
+    cart,
+  });
+};
+
+export const removeFromCart = async (req, res, next) => {
+  const { productId } = req.params;
+  const userId = req.authUser._id;
+
+  const cart = await Cart.findOne({ userId, "products.productId": productId });
+  if (!cart) {
+    return next(new ErrorClass("user doesn't have cart", 400));
+  }
+
+  cart.products = cart.products.filter((p) => {
+    p.productId !== productId;
+  });
+
+  if (cart.products.length == 0) {
+    await Cart.deleteOne({ userId });
+    return res.status(200).json({
+      status: "Success",
+      message: "Product removed from cart Successfully",
+    });
+  }
+
+  cart.subTotal = cart.products.reduce(
+    (total, p) => total + p.quantity * p.price,
+    0
+  );
+
+  await cart.save();
+  return res.status(200).json({
+    status: "Success",
+    message: "Product removed from cart Successfully",
+    cart,
+  });
+};
+
+export const getCart = async (req, res, next) => {
+  const userId = req.authUser._id;
+
+  const cart = await Cart.findOne({ userId });
+
+  return res.status(200).json({
+    status: "Success",
+    message: "Cart fetched Successfully",
+    cart,
+  });
+};
--- src/modules/Cart/cart.routes.js
+++ src/modules/Cart/cart.routes.js
@@ -1,10 +1,20 @@
 import { Router } from "express";
 
 import * as middlewares from "../../Middlewares/index.js";
-import { addCart } from "./cart.controller.js";
+import {
+  addCart,
+  getCart,
+  removeFromCart,
+  updateCart,
+} from "./cart.controller.js";
 
 const { auth, errorHandler } = middlewares;
 
 export const cartRouter = Router();
 
 cartRouter.post("/add/:productId", auth(), errorHandler(addCart));
+
+cartRouter.put("/update/:productId", auth(), errorHandler(updateCart));
+cartRouter.put("/remove/:productId", auth(), errorHandler(removeFromCart));
+
+cartRouter.get("/", auth(), errorHandler(getCart));
